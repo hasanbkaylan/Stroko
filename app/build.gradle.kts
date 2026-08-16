484140f23d6d81098983b9398767dd6af33f2046
hasanbkaylan <msalogrenci@gmail.com>
2026-08-16 20:57:42 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,12 @@ android {
       keyAlias = "upload"
       keyPassword = System.getenv("KEY_PASSWORD")
     }
+    create("debugConfig") {
+      storeFile = file("${rootDir}/debug.keystore")
+      storePassword = "android"
+      keyAlias = "androiddebugkey"
+      keyPassword = "android"
+    }
   }
 
   buildTypes {
@@ -41,22 +47,17 @@ android {
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
       signingConfig = signingConfigs.getByName("release")
     }
-    debug {
-    }
+    debug { signingConfig = signingConfigs.getByName("debugConfig") }
   }
-
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
   }
-
   buildFeatures {
     compose = true
     buildConfig = true
   }
-
   testOptions { unitTests { isIncludeAndroidResources = true } }
-
   dependenciesInfo {
     includeInApk = false
     includeInBundle = true
